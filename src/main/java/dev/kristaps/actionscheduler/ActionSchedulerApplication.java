8adb117f59304a1e6b1dd932123391d535408b74
DeKristaps <dekristaps@gmail.com>
2023-01-11 12:46:39 +0200

    Initialized project for a ActionScheduler

---
+++ src/main/java/dev/kristaps/actionscheduler/ActionSchedulerApplication.java
@@ -0,0 +1,13 @@
+package dev.kristaps.actionscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionSchedulerApplication.class, args);
+    }
+
+}
